524bd7870abea0dbdb09aa05b04e7105867114f0
Breakdown08 <nicegameafk@gmail.com>
2023-12-06 11:46:51 +0700

    препроцесс

--- main.py
+++ main.py
@@ -11,14 +11,31 @@ import random
 random.seed(33)
 torch.manual_seed(0)
 reviews = []
+texts = []
+labels = []
+
+
+def preprocess(text):
+    """"
+    Функция чтобы очистить текст отзыва от пунктуации и выделить все слова
+    """
+    clear_text = "".join([s for s in text if s not in punctuation])  # убираем пунктуацию
+    text_words = clear_text.split()  # получаем массив слов
+    return clear_text, text_words
+
 
 # Считываем данные из файлов
 with open('dataset.csv', 'r', encoding='utf-8') as f:
     dataset = f.read().split('\n')
 
-for item in dataset[1:4 + 1]:
-    #print(item)
+for item in dataset[1:4-1]:
     reviews.append(Review(item))
 
 for review in reviews:
-    print(review.text)
+    print(preprocess(review.text))
+    texts.append(review.text)
+    labels.append(review.label)
+
+
+#all_reviews, all_words = preprocess(texts)
+#print('Общее число отзывов: ', len(all_reviews))
